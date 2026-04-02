da60e89b01d9277b8f788fed79e35c89dc01e4f8
Unselfisheologism <jeffrinjames99@gmail.com>
2026-04-02 07:37:58 +0530

    logssyntax error

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
 }
 
